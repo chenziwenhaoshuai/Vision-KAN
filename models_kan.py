811d05c0525057f907eac2a9f968acb55a34ba0d
Ziwen Chen <75919080+chenziwenhaoshuai@users.noreply.github.com>
2024-05-17 13:35:49 +0800

    Update models_kan.py

--- models_kan.py
+++ models_kan.py
@@ -31,7 +31,7 @@ class kanBlock(Block):
         # self.norm2 = norm_layer(dim)
         # mlp_hidden_dim = int(dim * mlp_ratio)
         # self.mlp = Mlp(in_features=dim, hidden_features=mlp_hidden_dim, act_layer=act_layer, drop=drop)
-        self.kan = KAN([dim, 20, dim])
+        self.kan = KAN([dim, 192, dim])
 
     def forward(self, x):
         b, t, d = x.shape
